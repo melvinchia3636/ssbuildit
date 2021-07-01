a3ce7d166c7ed24c23e7eb2517e30237fc006460
melvinchia3636 <melvinchia623600@gmail.com>
2021-07-01 14:36:38 +0800

    first update

---
+++ scrape.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+DOMAIN = "https://www.scbuildit.hubsinfo.net"
+
+lst = list(range(7))
+
+json.dump([[i.text, (list(map(lambda row: (lambda name, level, time, max, mats, used, desc: [*[name.find('img')['src'], name.text.strip(), int(level.text), int(time.text), int(max.text), [[i.select_one('img')['src'], int(i.text.replace('x', '')), i.select_one('img')['alt']] for i in mats.select('.list-mats')], [[i['src'], i['alt']] for i in used.select('img')], desc.text]])(*row) if len(row) > 0 else [], [i.select('td') for i in bs(requests.get(DOMAIN+i['href']).content, 'lxml').select('table tr')[1:]])))] for i in bs(requests.get(DOMAIN).content, 'lxml').select('ul.dropdown-menu li a')[1:]], open('result.json', 'w'))
---
+++ script.js
@@ -0,0 +1,39 @@
+const DOMAIN = "https://www.scbuildit.hubsinfo.net";
+const COLORMAP = ["gray-500", "orange-500", "indigo-500", "green-300", "yellow-800", "green-500", "pink-500", "purple-300", "yellow-400", "blue-500", , , , , , , , "teal-400"]
+
+const getCMAPIndex = name => {
+	const index = data.map(e => e[1].some(e => e[1] === name.replace('Bread', "Bread Roll").replace("Bags", "Bag").replace('Fruit Berries', 'Fruit and Berries'))).indexOf(true)
+	return index !== -1 ? index : console.log(name)
+}
+
+$('main').append(data.map(([title, elements], index) => `
+	<div>
+		<h1 class="text-center font-extrabold text-4xl mb-8">${title}</h1>
+		<div class="grid items-center justify-center gap-5 mb-32 px-20" style="grid-template-columns: repeat(4, 25%)">
+			${elements.map(([img, name, level, time, maxVal, mats, used, desc]) => `
+				<div class="bg-white rounded-xl flex flex-col items-center p-6 relative" style="box-shadow: 0 4px 4px rgba(0, 0, 0, .25); height: ${elements.map(e => e[5].length > 0).some(e => e) ? "40em" : "28em"}">
+					<h3 class="font-extrabold text-lg mb-3">LEVEL ${level}</h3>
+					<div class="w-32 h-32 rounded-full bg-${COLORMAP[index]}" style="padding: .4em; box-shadow: inset 0 2px 6px rgba(0, 0, 0, .6)"><img src="${DOMAIN+img}" class="bg-white rounded-full p-5" style="box-shadow: 0 2px 4px rgba(0, 0, 0, .6)"></div>
+					<h2 class="font-extrabold text-3xl pt-4 text-center leading-tight">${name}</h2>
+					<div class="font-extrabold flex items-center mt-1"><img src="https://www.scbuildit.hubsinfo.net/images/icons/misc-icons/currency_coins.png" class="w-5 mr-1">${maxVal}</div>
+					<p class="font-bold text-xl text-center mt-5 text-gray-900 leading-tight">${desc}</p>
+					${elements.map(e => mats.length > 0).some(e => e) ? `
+						<h3 class="font-extrabold text-lg mb-3 mt-8">Materials</h3>
+						<div class="flex">
+							${mats.map(([i, a, n]) => `
+							<div class="flex items-center justify-center w-12 h-12 mx-1 rounded-full bg-${COLORMAP[getCMAPIndex(n)]}" style="box-shadow: inset 0 2px 6px rgba(0, 0, 0, .6)"><img src="${DOMAIN+i}" class="bg-white rounded-full p-2 w-10 h-10" style="box-shadow: 0 2px 4px rgba(0, 0, 0, .6)"></div>
+							`).join("")}
+						</div>` : ""}
+					${elements.map(e => used.length > 0).some(e => e) ? `
+						<h3 class="font-extrabold text-lg mb-3 mt-4">Used In</h3>
+						<div class="flex">
+							${used.map(([i, n]) => `
+							<div class="flex items-center justify-center w-12 h-12 mx-1 rounded-full bg-${COLORMAP[getCMAPIndex(n)]}" style="box-shadow: inset 0 2px 6px rgba(0, 0, 0, .6)"><img src="${DOMAIN+i}" class="bg-white rounded-full p-2 w-10 h-10" style="box-shadow: 0 2px 4px rgba(0, 0, 0, .6)"></div>
+							`).join("")}
+						</div>` : ""}
+					<h3 class="font-extrabold absolute left-50" style="bottom: 1.4em">MIN. ${time} minutes</h3>
+				</div>
+			`).join("")}
+		</div>
+	</div>
+`));
